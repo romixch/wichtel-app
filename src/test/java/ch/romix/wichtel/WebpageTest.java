dea664b4ae8423d6f8a2c6d1509bd36428f2b5de
Roman Schaller <roman.schaller@gmail.com>
2014-12-28 18:23:14 +0100

    Add test to check if index.html is deliverd at the root context.

---
+++ src/test/java/ch/romix/wichtel/WebpageTest.java
@@ -0,0 +1,39 @@
+package ch.romix.wichtel;
+
+import java.net.URI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.Matchers.is;
+
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WichtelApp.class)
+@WebAppConfiguration
+@IntegrationTest({"server.port=0"})
+public class WebpageTest {
+
+  @Value("${local.server.port}")
+  private int port;
+
+  @Test
+  public void testIfIndexHtmlIsServed() throws Exception {
+    RestTemplate template = new TestRestTemplate();
+    ClientHttpRequest request = template.getRequestFactory().createRequest(new URI("http://localhost:" + port + "/"), HttpMethod.GET);
+    ClientHttpResponse response = request.execute();
+    assertThat(response.getStatusCode(), is(HttpStatus.OK));
+  }
+}
